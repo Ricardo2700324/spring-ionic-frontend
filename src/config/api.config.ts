2c2333e07b3854f7845d023a19db263feecdc8e8
Ricardo Fernando de Souza <rfs.br@hotmail.com>
2019-06-24 10:28:16 -0300

    Mostrando as categorias

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,3 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8091"
+    baseUrl: "http://localhost:8091",
+    bucketBaseUrl: "https://spring-ionic-primeiro.s3-sa-east-1.amazonaws.com"
 }
